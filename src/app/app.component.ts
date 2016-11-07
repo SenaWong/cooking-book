a10a4bb8c530357567a2c0b31f700b690a73ada9
Edmund Wong <wongkokliong99@hotmail.com>
2016-11-08 02:04:36 +0800

    Done header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.font-awesome.min.css','./app.component.style.min.css', './app.component.main.default.css']
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  register(){
+    window.open("register.html", "register", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=550");
+  }
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+
+}
